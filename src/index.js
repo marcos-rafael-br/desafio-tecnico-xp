7fef1418a54145f00866d4ad20bac00e40c69128
marcos-rafael-br <mrpf10@ehotmail.com>
2022-07-22 05:29:18 -0300

    feat::teste deploy acertando variavel PORT no index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import app from './app.js';
 import dotenv from 'dotenv';
+import app from './app.js';
 
 dotenv.config();
 
-const PORT = process.env.MYSQL_PORT 
+const PORT = process.env.MYSQL_PORT;
 
-const server = app.listen(PORT , () => console.log(
+const server = app.listen(PORT || 5000, () => console.log(
   `Server is running on PORT: ${PORT}`,
 ));
 
