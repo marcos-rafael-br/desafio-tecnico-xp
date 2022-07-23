7ed0abc031bffee64274c120949f2dd110cce888
marcos-rafael-br <mrpf10@ehotmail.com>
2022-07-23 20:50:42 -0300

    chore: :hammer: Adição de script para executar varredura do ESlint

--- src/shared/http.exception.js
+++ src/shared/http.exception.js
@@ -1,10 +1,10 @@
 class HttpException extends Error {
-    status
-    constructor(status, message) {
-        super(message);
-        this.status = status;
-      
-    }
+  status;
+
+  constructor(status, message) {
+    super(message);
+    this.status = status;
+  }
 }
 
 export default HttpException;
