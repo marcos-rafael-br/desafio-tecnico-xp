21d9c430653b15212e5c3854354222ed8e41130f
marcos-rafael-br <mrpf10@ehotmail.com>
2022-07-24 00:34:23 -0300

    fix: :bug: [camada venda.service] - Possiblidade de haver vendas(sem custodia do ativo) se cliente vender fracionado

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -6,7 +6,7 @@ const { sign, verify } = pkg;
 const TOKEN_SECRET = process.env.TOKEN_SECRET || 'sonhogrande';
 
 const jwtConfig = {
-  expiresIn: '30m',
+  expiresIn: '60m',
   algorithm: 'HS256',
 };
 const generateJWTToken = (user) => sign({ user }, TOKEN_SECRET, jwtConfig);
